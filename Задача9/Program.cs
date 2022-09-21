d4de38554d2f94c97365bccfe49b4263026f8169
Konstantin <kzhestovskih2012@gmail.com>
2022-09-21 21:33:29 +0500

    Задача 9

---
+++ Задача9/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Число {num}");
+int num1 = num / 10;
+int num2 = num % 10;
+if (num1 > num2){
+    Console.WriteLine($"цифра {num1} > больше цифры {num2}");
+} else {
+    Console.WriteLine($"цифра {num2} > больше цифры {num1}");
+}
