77ef262faf7f0592bd2c8308cfde84053994b0c0
Konstantin <kzhestovskih2012@gmail.com>
2022-09-21 23:32:56 +0500

    Задача 16 - 1

---
+++ Задача 16/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a * a == b){
+    Console.WriteLine($"Число {b} является квадратом числа {a}");
+}
+if (b * b == a){
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+} else{
+    Console.WriteLine("Нет");
+}
